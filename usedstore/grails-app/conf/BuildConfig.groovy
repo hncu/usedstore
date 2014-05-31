076b7ca99feb28dd2529c353ce54a1bd35576cff
wanderknight <wanderknight@gmail.com>
2014-05-31 14:39:19 +0800

    3. grails create-app usedstore

--- usedstore/grails-app/conf/BuildConfig.groovy
+++ usedstore/grails-app/conf/BuildConfig.groovy
@@ -57,7 +57,7 @@ grails.project.dependency.resolution = {
         build ":tomcat:7.0.52.1"
 
         // plugins for the compile step
-        compile ":scaffolding:2.0.2"
+        compile ':scaffolding:2.0.2'
         compile ':cache:1.1.1'
 
         // plugins needed at runtime but not for compilation
